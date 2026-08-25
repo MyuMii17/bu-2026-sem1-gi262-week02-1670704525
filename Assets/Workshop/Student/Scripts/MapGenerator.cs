ee86684b02833994d9bb87ab4d786e4cbc96bf3f
Ratsarun Thipdumrong <mewgamer17@gmail.com>
2026-08-25 18:38:46 +0700

    WorkShop Cps

--- Assets/Workshop/Student/Scripts/MapGenerator.cs
+++ Assets/Workshop/Student/Scripts/MapGenerator.cs
@@ -8,39 +8,36 @@ namespace Workshop.Student
     {
         public int columns = 10;
         public int rows = 10;
-        public GameObject[] Players;
+        public GameObject[] players;
         public GameObject exitPrefeb;
 
         public GameObject[] floorTiles;
         public GameObject[] wallTiles;
         public GameObject[] foodTiles;
-        public HashSet<Transform> foodPos = new HashSet<Transform>();
+        public HashSet<Vector3> obstaclePos = new HashSet<Vector3>();
 
         public string[,] saveItemMap = new string[3, 3] {
             { " ", "Soda", " "},
             { " ", " ", " "},
-            { " ", " ", "Food"},
+            { "Food", " ", " "},
         };
-
-        // 1. declare Players variable
-
-        // 7. declare Exit variable 
 
 
         public void Start()
         {
             // 1. random player at the position <0, 0> map
-            int playerIndex = UnityEngine.Random.Range(0, Players.Length);
-            Instantiate(Players[playerIndex], new Vector2(0, 0), Quaternion.identity);
+            int playerIndex = UnityEngine.Random.Range(0, players.Length);
+            GameObject player = Instantiate(players[playerIndex], new Vector3(0, 0), Quaternion.identity);
 
             // 2. create obstacles
-            for (int y = 2; y < rows; y++)
+            for (int y = 1; y < rows - 1; y++)
             {
-                for (int x = 4; x < columns; x++)
+                for (int x = 1; x < columns - 1; x++)
                 {
                     int r = UnityEngine.Random.Range(0, wallTiles.Length);
-                    GameObject floor = Instantiate(wallTiles[r], new Vector3(x, y), Quaternion.identity);
-                    floor.name = $"Wall({x},{y})";
+                    GameObject obstacle = Instantiate(wallTiles[r], new Vector3(x, y), Quaternion.identity);
+                    obstacle.name = $"Obstacle({x},{y})";
+                    obstaclePos.Add(obstacle.transform.position);
                 }
             }
 
@@ -69,15 +66,27 @@ namespace Workshop.Student
                 }
             }
 
+            // 7. place exit
+            GameObject exit = Instantiate(exitPrefeb, new Vector3(9, 9), Quaternion.identity);
+
             // 5. random foods
-            int numberOfFood = UnityEngine.Random.Range(1, 3);
+            int numberOfFood = UnityEngine.Random.Range(1, 6);
             for (int i = 0; i < numberOfFood; i++)
             {
-                int randomX = UnityEngine.Random.Range(0, columns);
-                int randomY = UnityEngine.Random.Range(0, rows);
-                int f = UnityEngine.Random.Range(0, foodTiles.Length);
-                GameObject food = Instantiate(foodTiles[f], new Vector2(randomX, randomY), Quaternion.identity);
-                food.name = $"Food({randomX},{randomY})";
+                bool isCanSpawnFood = true;
+                while (isCanSpawnFood)
+                {
+                    int randomX = UnityEngine.Random.Range(0, columns);
+                    int randomY = UnityEngine.Random.Range(0, rows);
+                    int f = UnityEngine.Random.Range(0, foodTiles.Length);
+                    Vector3 foodPos = new Vector3(randomX, randomY);
+                    if (!obstaclePos.Contains(foodPos) && foodPos != player.transform.position && foodPos != exit.transform.position)
+                    {
+                        GameObject food = Instantiate(foodTiles[f], foodPos, Quaternion.identity);
+                        food.name = $"Food({randomX},{randomY})";
+                        isCanSpawnFood = false;
+                    }
+                }
             }
 
             // 6. generate item along with the saveItemMap
@@ -100,9 +109,6 @@ namespace Workshop.Student
                     }
                 }
             }
-
-            // 7. place exit
-            Instantiate(exitPrefeb, new Vector2(9, 9), Quaternion.identity);
         }
     }
 
